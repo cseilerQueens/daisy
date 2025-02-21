c9e97f648657dd54f98f2746d49d462603007ed3
Christian Seiler <christian.seiler@queensu.ca>
2025-02-21 16:27:23 -0500

    update

--- install_daisy.R
+++ install_daisy.R
@@ -1,4 +1,4 @@
-# module load StdEnv/2023 gcc/12.3 udunits/2.2.28 hdf/4.2.16 gdal/3.7.2 r/4.3.1
+# module load StdEnv/2023 gcc/12.3 udunits/2.2.28 hdf/4.2.16 gdal/3.7.2 r/4.3.1 meta-farm/1.0.2
 
 setwd("/home/cseiler/daisy")
 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-module load  StdEnv/2023  gcc/12.3  udunits/2.2.28  hdf/4.2.16  gdal/3.7.2  r/4.3.1
+module load  StdEnv/2023  gcc/12.3  udunits/2.2.28  hdf/4.2.16  gdal/3.7.2  r/4.3.1 meta-farm/1.0.2
 
 # Install renv
 R -e "install.packages("renv")"
@@ -24,3 +24,4 @@ R -e "install.packages('roxygen2', repos='https://mirror.csclub.uwaterloo.ca/CRA
 R -e "install.packages('GA', repos='https://mirror.csclub.uwaterloo.ca/CRAN/', dep=TRUE)"
 R -e "install.packages('latex2exp', repos='https://mirror.csclub.uwaterloo.ca/CRAN/', dep=TRUE)"
 R -e "install.packages(c('shiny', 'foreach', 'slickR'), repos='https://mirror.csclub.uwaterloo.ca/CRAN/', dep=TRUE)"
+R -e "install.packages('readr', repos='https://mirror.csclub.uwaterloo.ca/CRAN/', dep=TRUE)"
